a44431de8d3da281a9e1a3ae98f373fcebeccfbd
jowoojin <codewrite123@gmail.com>
2025-02-26 16:35:07 +0900

    gemini api use

--- make_chord_project/pybo/views/main_views.py
+++ make_chord_project/pybo/views/main_views.py
@@ -2,7 +2,30 @@ from flask import Blueprint,send_file
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 
+import os
+import google.generativeai as genai
+from flask import request, jsonify
+from dotenv import load_dotenv
+
+
 bp = Blueprint('main', __name__, url_prefix='/')
+
+
+def generate_text():
+    load_dotenv() # .env 파일 로드
+    api_key = os.getenv("GEMINI_API_KEY") # 환경 변수에서 API 키 가져오기
+
+    if not api_key:
+        print("API 키가 설정되지 않았습니다. .env 파일을 확인하세요.")
+        return
+
+    system_instruction="""기타 코드 운지법에 대해 질문을 하면 어떤 말을 할 필요도 없다. 오로지 좌표값으로만 출력을 해줘야 한다. 예를 들어 G코드 운지법에 대해 물어보면 2번프랫의 5번줄을 잡으니까 (2,5)이렇게 적고 프랫이 바뀔때만 /n로 줄바꿈을 해준다. 
+    프랫이 바뀌지 않을 경우 (1,2)/(1,3)/(1,4) 이런식으로 /(슬래쉬)를 사용해서 구분한다. 마지막으로 뮤트할 줄이 존재한다면 맨 마지막 줄에 mute: 이런식으로 적고 뮤트되어야 할 줄을 숫자로 입력해준다. 만약 존재하지 않는다면 없음이라는 단어를 출력하면된다. 똑같은 질문을 반복하더라도 정확하게 기타 코드의 운지법을 알려줘야 한다. """
+    
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel('gemini-1.5-flash',system_instruction=system_instruction)
+    response = model.generate_content("f코드 운지법")
+    print(response.text)
 
 
 @bp.route('/')
@@ -75,11 +98,11 @@ def create_fretboard():
      
     coordinates = create_coordinate_system()
     
-    gpt_response = """(1,1)/(1,2)/(1,3)/(1,4)/(1,5)
+    gpt_response = """(1,1)/(1,2)/(1,3)/(1,4)/(1,5)/(1,6)
 (3,3)
-(3,4)
-(3,6)
-mute: 6"""
+(3,5)
+(2,4)
+mute:없음"""
     
     points_to_draw ,mute, first_x= parse_gpt_response(gpt_response)
     
@@ -105,5 +128,7 @@ mute: 6"""
     img_io = BytesIO()
     img.save(img_io, 'PNG')
     img_io.seek(0)
+    
+    #generate_text()
     
     return send_file(img_io, mimetype='image/png')
